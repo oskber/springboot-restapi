9af32d4519d39ac4bad0c7c161d7efb8d038c982
Oskar Bergman <ocke_bergman@hotmail.com>
2024-11-28 14:52:30 +0100

    OAuth2 security

--- src/main/java/org/example/springbootrestapi/security/Security.java
+++ src/main/java/org/example/springbootrestapi/security/Security.java
@@ -2,6 +2,8 @@ package org.example.springbootrestapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,15 +14,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.http.HttpMethod.*;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity
 public class Security {
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,13 +35,12 @@ public class Security {
                         authorize
                                 .requestMatchers(GET, "/categories/**").permitAll()
                                 .requestMatchers(GET, "/index.html", "/js/**").permitAll()
-                                .requestMatchers(POST, "/categories").hasRole("ADMIN")
-                                .requestMatchers(POST, "/categories/**").authenticated()
+                                .requestMatchers(POST, "/categories").hasAuthority("admin")
                                 .requestMatchers(GET, "/locations/**").permitAll()
                                 .requestMatchers(GET, "/locations/category/**").permitAll()
-                                .requestMatchers(GET, "/locations/user/**").hasRole("USER")
-                                .requestMatchers(PUT, "/locations/**").hasRole("USER")
-                                .requestMatchers(POST, "/locations").hasRole("USER")
+                                .requestMatchers(GET, "/locations/user/**").hasAuthority("user")
+                                .requestMatchers(PUT, "/locations/**").hasAuthority("user")
+                                .requestMatchers(POST, "/locations").hasAuthority("user")
                                 .requestMatchers(GET, "/locations/area/**").permitAll()
                                 .requestMatchers(
                                         "/v3/api-docs",
@@ -53,10 +57,20 @@ public class Security {
                                 .anyRequest().denyAll())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
         return http.build();
+    }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("scope");
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtConverter;
     }
 
     @Bean
@@ -65,13 +79,13 @@ public class Security {
         UserDetails user = User.builder()
                 .username("user")
                 .password(encoder.encode("password"))
-                .roles("USER")
+                .authorities("USER")
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(encoder.encode("password"))
-                .roles("USER", "ADMIN")
+                .authorities("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
